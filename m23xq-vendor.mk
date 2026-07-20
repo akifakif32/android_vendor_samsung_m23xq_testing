b92501cbc1438c3cfa4a4f761c97f1fc731d3db6
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-20 20:36:21 +0300

    fix

--- m23xq-vendor.mk
+++ m23xq-vendor.mk
@@ -126,6 +126,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/m23xq/proprietary/vendor/lib64/camera/w_tof_calibration.bin:$(TARGET_COPY_OUT_VENDOR)/lib64/camera/w_tof_calibration.bin
 
 PRODUCT_PACKAGES += \
+    libbauthserver \
     sensors.grip \
     sensors.ssc \
     audio.primary.lito \
@@ -195,7 +196,6 @@ PRODUCT_PACKAGES += \
     camera.qcom \
     com.qti.chi.override \
     lights.lito \
-    libbauthserver \
     libbauthtzcommon \
     libegis_fp_normal_sensor_test \
     libgf_in_system_lib \
